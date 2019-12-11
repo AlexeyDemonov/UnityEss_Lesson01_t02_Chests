02ca4f1a3b45e782aa3543c5962a602330bba21a
alexei.demonov.341 <alexei.demonov.341@gmail.com>
2019-12-11 12:43:49 +0500

    First commit Added: CustomHotKeysSetter and GameQuitter from previous projects Added: ChestClicker - script to open the chest by clicking on it

---
+++ Assets/Editor/CustomHotkeysSetter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CustomHotkeysSetter
+{
+    [MenuItem("Edit/CustomHotkeys/PlayOrStop _F5")]
+    static void PlayOrStopGame()
+    {
+        if (!EditorApplication.isPlaying)
+            EditorApplication.ExecuteMenuItem("File/Save");
+
+        EditorApplication.ExecuteMenuItem("Edit/Play");
+    }
+
+    [MenuItem("GameObject/CustomHotkeys/ResetTransform #R")]
+    static void ResetTransform()
+    {
+        var selectedObject = Selection.activeGameObject;
+
+        if(selectedObject != null)
+        {
+            selectedObject.transform.position = Vector3.zero;
+            selectedObject.transform.rotation = Quaternion.identity;
+            selectedObject.transform.localScale = Vector3.one;
+        }
+    }
+}
